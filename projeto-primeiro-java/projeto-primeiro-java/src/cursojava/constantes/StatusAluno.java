926d14b0fdacadb0cc452d9a3a5e6902e0180445
Rafael Vinicius da Silva Basilio <108074472+RafaelViniciusdaSilvaBasilio@users.noreply.github.com>
2024-06-15 11:22:26 -0300

    Add files via upload

---
+++ projeto-primeiro-java/projeto-primeiro-java/src/cursojava/constantes/StatusAluno.java
@@ -0,0 +1,9 @@
+package cursojava.constantes;
+
+/*Classes e constatntes estáticas*/
+public class StatusAluno {
+
+	public static String APROVADO = "Aprovado";
+	public static String RECUPERAÇAO = "Recuperação";
+	public static String REPROVADO = "Reprovado";
+}
