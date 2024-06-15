e1228a0b16486cfacf8e6336a1abe77edc94f5fa
Rafael Vinicius da Silva Basilio <108074472+RafaelViniciusdaSilvaBasilio@users.noreply.github.com>
2024-06-15 11:25:57 -0300

    Delete projeto-primeiro-java directory

--- projeto-primeiro-java/projeto-primeiro-java/src/cursojava/constantes/StatusAluno.java
+++
@@ -1,9 +0,0 @@
-package cursojava.constantes;
-
-/*Classes e constatntes estáticas*/
-public class StatusAluno {
-
-	public static String APROVADO = "Aprovado";
-	public static String RECUPERAÇAO = "Recuperação";
-	public static String REPROVADO = "Reprovado";
-}
